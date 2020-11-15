33148c87b9c6758b593e82b6e8cdfa9166f6415b
macricek <marko.chylik@gmail.com>
2020-11-15 17:44:35 +0100

    markob

--- Src/tim.c
+++ Src/tim.c
@@ -43,21 +43,22 @@ void MX_TIM2_Init(void)
   TIM_InitStruct.Autoreload = 99;									//	ked tak zmenit
   TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;			// ok
   LL_TIM_Init(TIM2, &TIM_InitStruct);								// ok
-  LL_TIM_DisableARRPreload(TIM2);							//V masterovi je to zakomentovane (in case)
-  //LL_TIM_EnableARRPreload(TIM2);
+  //LL_TIM_DisableARRPreload(TIM2);							//V masterovi je to zakomentovane (in case)
+  LL_TIM_EnableARRPreload(TIM2);
   LL_TIM_SetClockSource(TIM2, LL_TIM_CLOCKSOURCE_INTERNAL);			// ok
   LL_TIM_OC_EnablePreload(TIM2, LL_TIM_CHANNEL_CH1);				// toto neni v masterovi
   TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_PWM1;
   TIM_OC_InitStruct.OCState = LL_TIM_OCSTATE_DISABLE;
   TIM_OC_InitStruct.OCNState = LL_TIM_OCSTATE_DISABLE;
 
-  TIM_OC_InitStruct.CompareValue = 99;
+  TIM_OC_InitStruct.CompareValue = 0;
   TIM_OC_InitStruct.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
   LL_TIM_OC_Init(TIM2, LL_TIM_CHANNEL_CH1, &TIM_OC_InitStruct);
   LL_TIM_OC_DisableFast(TIM2, LL_TIM_CHANNEL_CH1);
 
 
   //CH2
+  //LL_TIM_SetPrescaler(TIM2, __LL_TIM_CALC_PSC(SystemCoreClock, 10000));
   TIM_InitStruct.Prescaler = 7999;									//1Khz
   TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
   TIM_InitStruct.Autoreload = 9;									//	10ms
@@ -71,10 +72,14 @@ void MX_TIM2_Init(void)
   LL_TIM_OC_Init(TIM2, LL_TIM_CHANNEL_CH2, &TIM_OC_InitStruct);
   LL_TIM_OC_DisableFast(TIM2, LL_TIM_CHANNEL_CH2);
   LL_TIM_SetTriggerOutput(TIM2, LL_TIM_TRGO_RESET);
-  LL_TIM_DisableMasterSlaveMode(TIM2);								//ok
+  LL_TIM_DisableMasterSlaveMode(TIM2);			 					//ok
+
+  LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
   LL_TIM_EnableIT_UPDATE(TIM2);
+  LL_TIM_EnableIT_CC1(TIM2);
   LL_TIM_EnableCounter(TIM2);
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);				//ok
+  LL_TIM_GenerateEvent_UPDATE(TIM2);
 
 
     /**TIM2 GPIO Configuration
@@ -82,11 +87,12 @@ void MX_TIM2_Init(void)
     */
   GPIO_InitStruct.Pin = LL_GPIO_PIN_5;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   GPIO_InitStruct.Alternate = LL_GPIO_AF_1;
   LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  LL_GPIO_SetAFPin_0_7(GPIOA, LL_GPIO_PIN_5, LL_GPIO_AF_5);
 }
 
 /* USER CODE BEGIN 1 */
